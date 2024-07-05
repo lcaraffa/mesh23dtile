ba5b4d9701c0ae03a0f8726c6a57399f4cb34774
Antonin22 <antonin.ollierseedo.fr@gmail.com>
2024-07-05 14:54:39 +0200

    quote fix + offeset for obj-tiler

--- run_lidarhd.sh
+++ run_lidarhd.sh
@@ -3,6 +3,7 @@
 input_dir="${PWD}/datas/lidar_hd_crop"
 output_dir="${PWD}/output_lidarhd_crop"
 coords="635471.0x6856430.0"
+offset="635471.0 6856430.0"
 mode_proj=0 # convert obj file in b3dm and add a 4x4 transformation matrix in the tileset.json
 #mode_proj=1 # first convert obj in epsg:4979 and write the bounding volume as region in epsg:4978 coordinates
 tileset="${PWD}/3Dtile"
@@ -28,6 +29,6 @@ fi
 mkdir -p "${tileset}"
 for obj_file in "${output_dir}/tiles/"*.obj; do
     # mode_proj = 0:
-    obj-tiler -i "$obj_file" --offset "${coords}" --crs_in "EPSG:${input_crs}" --crs_out "EPSG:${output_crs}" -o "${tileset}"
+    obj-tiler -i "$obj_file" --offset ${offset} --crs_in EPSG:${input_crs} --crs_out EPSG:${output_crs} -o ${tileset}
     # TODO: mode_proj = 1
 done
